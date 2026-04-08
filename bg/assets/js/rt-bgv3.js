9aed5cb48349b6d8d8bf7c5daf3989497ad901b7
luizmello1969 <luiz.mello@w2off.com>
2026-04-08 15:42:16 -0400

    nh

--- bg/assets/js/rt-bgv3.js
+++ bg/assets/js/rt-bgv3.js
@@ -257,11 +257,6 @@
 
         if (clickId) {
             console.log('[VSL Tracker] ClickId found:', clickId);
-            // Append clickid to current page URL
-            if (!urlObj.searchParams.has('clickid')) {
-                urlObj.searchParams.set('clickid', clickId);
-                window.history.replaceState({}, '', urlObj.toString());
-            }
             captureMetaPixelParams();
             initTracking(clickId);
             setupViewContentHandler(clickId);
